b1a3e1850b369c485c40347bc584a02df77939e6
franceees <23103223@usc.edu.ph>
2024-05-29 13:37:18 +0800

    Initial commit

--- controllers/addImage.js
+++ controllers/addImage.js
@@ -8,7 +8,6 @@ const addImage = async (req, res) => {
         return res.json({ status: "error", error: "Please provide an image URL" });
     }
 
-    // Check if the Movie URL already exists in the database
     db.query('SELECT * FROM movies WHERE src = ?', [imageUrl], (error, results) => {
         if (error) {
             console.error("Error checking image:", error);
@@ -16,10 +15,9 @@ const addImage = async (req, res) => {
         }
 
         if (results.length > 0) {
-            // Movie already exists in the database
             return res.json({ status: "already_exists", message: "Movie already added" });
         } else {
-            // Insert the Movie URL into the database
+
             db.query('INSERT INTO movies (src) VALUES (?)', [imageUrl], (error, results) => {
                 if (error) {
                     console.error("Error adding image:", error);
